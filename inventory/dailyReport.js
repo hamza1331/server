5d80e9539c6af5d13a81fe92da4e38e0309aa14c
Ahaseeb98 <ahaseeb12251998@gmail.com>
2019-11-01 18:44:38 +0500

    daily report complete functionality added

--- inventory/dailyReport.js
+++ inventory/dailyReport.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Info = require("./../models/globalInfo");
-const FabricIssue = require("./../models/addFabricIssue");
+const DailyReport = require("./../models/addDailyReport");
 const Companies = require("./../models/AddCompanys");
 
 router.get("/getLoomWeavers", (req, res) => {
@@ -15,22 +15,21 @@ router.get("/getLoomWeavers", (req, res) => {
   });
 });
 
-router.get("/getFabricIssueInfo", (req, res) => {
+router.get("/getDailyReportInfo", (req, res) => {
   // sizing current record nos
-  Info.find({}, "fabricIssue_info").exec(function(err, data) {
+  Info.find({}, "dailyReport_info").exec(function(err, data) {
     if (err) {
         console.log("err", err);
       }
     res.json(data);
-  
   });
 });
 
-router.get("/getFabricByRecordNo", (req, res) => {
+router.get("/getDailyReportByRecordNo", (req, res) => {
   // gets sizing data by record Nos
   let record_no = req.query.record_no;
   // console.log('params / uprecord_no', record_no);
-  FabricIssue.findOne({ record_no: record_no }, (err, data) => {
+  DailyReport.findOne({ record_no: record_no }, (err, data) => {
     // console.log('data', data)
     if (err) {
       //manage error
@@ -42,41 +41,40 @@ router.get("/getFabricByRecordNo", (req, res) => {
   console.log("runned");
 });
 
-router.post("/addFabricIssue", (req, res) => {
+router.post("/addDailyReport", (req, res) => {
   // adds yarn Issue
   console.log("Add JJournals");
 
   console.log(req.body);
-  const fabricIssue = new FabricIssue(req.body);
+  const dailyReport = new DailyReport(req.body);
   Info.findByIdAndUpdate(
     "5d6a2eeef7935f12787d9cc6",
     {
       $inc: {
-        fabricIssue_info: 1
+        dailyReport_info: 1
       }
     },
     (err, data) => {
       console.log("inc", err, data);
-      fabricIssue
+      dailyReport
         .save()
         .then(x => {
           res.json(x);
         })
         .catch(err => {
           console.log("err", err);
-          res.status(500).send("err", err);
+          res.status(500).send(err);
         });
     }
   );
 });
 
-router.post("/editFabricIssue", (req, res) => {
+router.post("/editDailyReport", (req, res) => {
   // edits beam Nos
   console.log(req.body);
-  const { _id, inner, outer } = req.body;
-  FabricIssue.findOne({ _id: _id }, function(err, data) {
-    data.inner = inner;
-    data.outer = outer;
+  const { _id, arr } = req.body;
+  DailyReport.findOne({ _id: _id }, function(err, data) {
+    data.arr = arr;
     data.save();
     res.json(data);
   });
